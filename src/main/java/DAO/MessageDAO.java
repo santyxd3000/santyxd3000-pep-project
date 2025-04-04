00175391f55b08c913280ef49ccb98ddeded2cb6
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2025-04-04 23:07:29 +0000

    2 Methods in Service Implemented

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,75 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Message;
+import Util.ConnectionUtil;
+
 public class MessageDAO {
+
+        public List<Message> getAllMessages() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+        List<Message> messages = new ArrayList<>();
+        try {
+            connection = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message WHERE message_id = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+           rs = preparedStatement.executeQuery();
+           while (rs.next()){
+            Message message = new Message(rs.getInt("message_id"),rs.getInt("posted_by"),rs.getString("message_text"),rs.getInt("time_posted_epoch"));
+            messages.add(message);
+           }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (preparedStatement != null) preparedStatement.close();
+                
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return messages; 
+    }
+
+    public Message getMessageById(int message_id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+        try {
+            connection = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message WHERE message_id";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, message_id);
+            rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"), rs.getString("message_text"), rs.getInt("time_posted_epoch"));
+                return message;
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
     
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,6 +1,18 @@
 package Service;
 
+import Util.ConnectionUtil;
+import Model.Message;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageService {
+
+
 
     
     
