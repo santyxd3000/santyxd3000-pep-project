a911fd5b8a4e1cd6a28db1d9de84c255d9d1e350
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2025-04-05 01:30:44 +0000

    All DAO and Services Completed

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -40,13 +40,13 @@ public class MessageDAO {
         return messages; 
     }
 
-    public Message getMessageById(int message_id) {
+    public Message getMessageByMessageId(int message_id) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
         try {
             connection = ConnectionUtil.getConnection();
-            String sql = "SELECT * FROM message WHERE message_id";
+            String sql = "SELECT * FROM message WHERE message_id;";
             preparedStatement = connection.prepareStatement(sql);
 
             preparedStatement.setInt(1, message_id);
@@ -71,5 +71,132 @@ public class MessageDAO {
         }
         return null;
     }
+
+    public List<Message> getAllMessagesByAccountID(int account_id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+        List<Message> messages = new ArrayList<>();
+        try {
+            connection = ConnectionUtil.getConnection();
+            String sql = "SELECT * FROM message WHERE posted_by = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, account_id);
+            rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                Message message = new Message(rs.getInt("message_id"),
+                        rs.getInt("posted_by"),
+                        rs.getString("message_text"),
+                        rs.getInt("time_posted_epoch"));
+                messages.add(message);
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return messages; 
+    }
+
+    public Message insertMessage(Message message) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet pkeyResultSet = null; 
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, message.getPosted_by());
+            preparedStatement.setString(2, message.getMessage_text());
+            preparedStatement.setLong(3, message.getTime_posted_epoch());
+
+            preparedStatement.executeUpdate();
+
+            pkeyResultSet = preparedStatement.getGeneratedKeys();
+
+            if (pkeyResultSet.next()){
+                int generated_message_id = (int) pkeyResultSet.getInt(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (pkeyResultSet != null) pkeyResultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null; 
+    }
+
+    public Message updatMessage(int message_id, Message message) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = ConnectionUtil.getConnection();
+            String sql = "UPDATE message SET message_text = ? WHERE message_id = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, message.getMessage_text());
+            preparedStatement.setInt(2,message_id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    public Message deleteMessageById(int message_id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            Message message = getMessageByMessageId(message_id);
+            if (message == null){
+                return null;
+            }
+
+            connection = ConnectionUtil.getConnection();
+            String sql = "DELETE FROM message WHERE message_id = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1,message_id);
+            preparedStatement.executeUpdate();
+
+            return message;
+
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+
     
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,16 +1,73 @@
 package Service;
 
-import Util.ConnectionUtil;
+import Model.Account;
 import Model.Message;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import DAO.MessageDAO;
+import DAO.AccountDAO;
 import java.util.List;
 
 public class MessageService {
+    private MessageDAO messageDAO;
+    private AccountDAO accountDAO;
+
+    public MessageService() {
+        messageDAO = new MessageDAO();
+        accountDAO = new AccountDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO, AccountDAO accountDAO) {
+        this.messageDAO = messageDAO;
+        this.accountDAO = accountDAO;
+    }
+
+    public Message postMessage(Message message) throws IllegalArgumentException {
+        int postedBy = message.getPosted_by();
+        String text = message.getMessage_text();
+        long time = message.getTime_posted_epoch();
+
+        if (text == null || text.isBlank() || text.length()>255) {
+            throw new IllegalArgumentException("Message business rule error");
+        }
+
+        if (!accountDAO.accountExistsById(postedBy)){
+            throw new IllegalArgumentException("User does not exist");
+        }
+
+        return messageDAO.insertMessage(new Message(postedBy, text, time));
+    }
+
+    public List<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+    }
+
+    public Message getMessageById(int messageId) {
+        return messageDAO.getMessageByMessageId(messageId);
+    }
+
+    public List<Message> getMessagesByAccountId(int account_id){
+        return messageDAO.getAllMessagesByAccountID(account_id);
+    }
+
+    public Message updateMessage(int message_id, Message message) {
+        String text = message.getMessage_text();
+        if (text == null || text.isBlank() || text.length() > 255) {
+            throw new IllegalArgumentException("Text Business Rule Error");
+        }
+
+        Message existingMessage = messageDAO.getMessageByMessageId(message_id);
+        if (existingMessage == null) {
+            throw new IllegalArgumentException("No message found");
+        }
+
+        messageDAO.updatMessage(message_id,message);
+
+        return messageDAO.getMessageByMessageId(message_id);
+    }
+
+    public Message deleteMessage (int message_id){
+        return messageDAO.deleteMessageById(message_id);
+    }
+
 
 
 
